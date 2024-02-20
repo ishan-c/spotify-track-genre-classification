2e55481b07c7b4c336a04e328e4ca7b31ee83f27
Ishan <ishanchowdhur@gmail.com>
2024-02-20 12:41:21 -0800

    Add field mapping

--- integrations/spotify/endpoint_fields.py
+++ integrations/spotify/endpoint_fields.py
@@ -2,36 +2,34 @@ SPOTIFY_WEB_API = 'https://api.spotify.com/v1/'
 
 TRACK_DATA_ENDPOINT = 'tracks?ids='
 TRACK_DATA_FIELDS = {
-    'id': '',
-    'artists': '',
-    'duration_ms': '',
-    'name': '',
-    'popularity': ''
+    'id': 'track_id',
+    'duration_ms': 'duration_ms',
+    'name': 'track_name',
+    'popularity': 'track_populatiry'
 }
 ARTIST_FIELDS = {
-    'followers': '',
-    'genres': '',
-    'id': '',
-    'name': '',
-    'popularity': ''
+    'genres': 'artist_genres',
+    'id': 'artist_id',
+    'name': 'artist_name',
+    'popularity': 'artist_popularity'
 }
 
 FOLLOWERS_FIELDS = {
-    'total': ''
+    'total': 'artist_followers'
 }
 
 AUDIO_FEATURES_ENDPOINT = 'audio-features?ids='
 AUDIO_FEATURES_FIELDS = {
-    'acousticness': '',
-    'danceability': '',
-    'energy': '',
-    'instrumentalness': '',
-    'key': '',
-    'liveness': '',
-    'loudness': '',
-    'mode': '',
-    'speechiness': '',
-    'tempo': '',
-    'time_signature': '',
-    'valence': ''
+    'acousticness': 'acousticness',
+    'danceability': 'danceability',
+    'energy': 'energy',
+    'instrumentalness': 'instrumentalness',
+    'key': 'key',
+    'liveness': 'liveness',
+    'loudness': 'loudness',
+    'mode': 'mode',
+    'speechiness': 'speechiness',
+    'tempo': 'tempo',
+    'time_signature': 'time_signature',
+    'valence': 'valence'
 }
