a4465f85d5d59069ff0f05718331e13a518fcbfe
Ishan <ishanchowdhur@gmail.com>
2024-03-14 12:48:49 -0700

    Rebuild Experiment class to support logging and add several other features

--- evaluation/experiment.py
+++ evaluation/experiment.py
@@ -1,22 +1,65 @@
+from typing import Any, Dict, List, Optional
+
+import mlflow
+import numpy as np
+import torch.nn as nn
+from sklearn.base import BaseEstimator
+
 from evaluation.metrics import Metrics
 
 
 class Experiment:
-    def __init__(self, models: dict):
-        self.classifiers = models
+    def __init__(self, train_data: np.ndarray, train_labels: np.ndarray, test_data: np.ndarray, test_labels: np.ndarray,
+                 dataset_chars: dict, label_names: List[str]):
+        self.train_data = train_data
+        self.train_labels = train_labels
+        self.test_data = test_data
+        self.test_labels = test_labels
+        self.dataset_chars = dataset_chars
+        self.label_names = label_names
+
         self.results = {}
 
-    def run_experiment(self, X_train, y_train, X_test, y_test, labels):
+    def _run_model(self, name: str, model):
+        model.fit(self.train_data, self.train_labels)
+        predictions = model.predict(self.test_data)
+        metrics = Metrics(self.test_labels, predictions, self.label_names, name)
+        self.results[name] = metrics
 
-        for name, model in self.classifiers.items():
-            print(f'Training {name}...')
-            _ = model.fit(X_train, y_train)
-            predictions = model.predict(X_test)
-            metrics = Metrics(y_test, predictions, labels)
-            self.results[name] = metrics
-            print(f'Results for {name}:')
-            metrics.print_metrics()
+    def _log_model(self, name: str, model: Any, config: dict, model_tags: Dict[str, str], save_model: bool = False):
+        with mlflow.start_run():
+            mlflow.log_params(self.dataset_chars)
 
-    def get_metrics(self, classifier_name: str):
+            if 'base_model' in config:
+                mlflow.log_param('meta_model', config['model_type'])
+                mlflow.log_params(config['base_model'])
+            else:
+                mlflow.log_params(config)
 
-        return self.results.get(classifier_name)
+            mlflow.log_metrics(self.results[name].metrics)
+            if model_tags:
+                mlflow.set_tags(model_tags)
+
+            if save_model:
+                if isinstance(model, BaseEstimator):
+                    mlflow.sklearn.log_model(model, artifact_path="models/" + name, registered_model_name=name)
+                elif isinstance(model, nn.Module):
+                    mlflow.pytorch.log_model(model, artifact_path="models/" + name, registered_model_name=name)
+                else:
+                    print(f"Model type not supported for automatic MLflow logging: {type(model)}")
+
+    def run_experiment(self, models: Dict[str, Any], configs: Dict[str, Any], mlflow_path: Optional[str] = None,
+                       tags: Dict[str, Dict[str, str]] = None, save_models: bool = False):
+
+        if models.keys() != configs.keys():
+            print('Non-matching keys in `models` and `configs` input dictionaries. Please provide dictionaries with'
+                  ' corresponding keys')
+            return
+
+        if mlflow_path:
+            mlflow.set_experiment(mlflow_path)
+
+        for name, model in models.items():
+            self._run_model(name, model)
+            model_tags = tags.get(name, {})
+            self._log_model(name, model, configs[name], model_tags, save_models)
