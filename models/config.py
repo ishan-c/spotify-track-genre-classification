20b8b5ad3d653a5b49ceb8cd28c0fe12be613a22
Ishan <ishanchowdhur@gmail.com>
2024-04-01 15:51:56 -0700

    Add C=100 case to baseline model, also try changing solver

--- models/config.py
+++ models/config.py
@@ -108,3 +108,80 @@ baseline_10_C_experiment_configs = [
         }
     }
 ]
+
+baseline_100_C_experiment_configs = [
+    {
+        'model_type': 'skmultilearn.problem_transform.BinaryRelevance',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 100.0
+            }
+        }
+
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.ClassifierChain',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 100.0
+            }
+        }
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.LabelPowerset',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 100.0
+            }
+        }
+    }
+]
+
+baseline_saga_10_C_experiment_configs = [
+    {
+        'model_type': 'skmultilearn.problem_transform.BinaryRelevance',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 10.0,
+                'solver': 'saga'
+            }
+        }
+
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.ClassifierChain',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 10.0,
+                'solver': 'saga'
+            }
+        }
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.LabelPowerset',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 10.0,
+                'solver': 'saga'
+            }
+        }
+    }
+]
