c638b3e3288bc53da183d4d9970d5f0845876edf
Ishan <ishanchowdhur@gmail.com>
2024-04-01 13:19:27 -0700

    Add configs for scaling C parameter in logistic regression base model

--- models/config.py
+++ models/config.py
@@ -34,3 +34,77 @@ baseline_experiment_configs = [
         }
     }
 ]
+
+baseline_0_1_C_experiment_configs = [
+    {
+        'model_type': 'skmultilearn.problem_transform.BinaryRelevance',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 0.1
+            }
+        }
+
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.ClassifierChain',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 0.1
+            }
+        }
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.LabelPowerset',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 0.1
+            }
+        }
+    }
+]
+
+baseline_10_C_experiment_configs = [
+    {
+        'model_type': 'skmultilearn.problem_transform.BinaryRelevance',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 10.0
+            }
+        }
+
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.ClassifierChain',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 10.0
+            }
+        }
+    },
+    {
+        'model_type': 'skmultilearn.problem_transform.LabelPowerset',
+        'base_model': {
+            'model_type': 'sklearn.linear_model.LogisticRegression',
+            'hyperparameters': {
+                'max_iter': 1000,
+                'random_state': SEED,
+                'C': 10.0
+            }
+        }
+    }
+]
